6fdc2ff01847bee03a2d908dd49b6a52e3b098d5
BuildTools <unconfigured@null.spigotmc.org>
2017-03-02 23:00:36 +0800

    FIX: StaffCore.class ADDED: Logger in Utils

--- src/me/sehplayz/util/Utils.java
+++ src/me/sehplayz/util/Utils.java
@@ -3,6 +3,8 @@ package me.sehplayz.util;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
+import java.util.logging.Level;
+
 /**
  * Created by sehplayz.
  */
@@ -16,5 +18,9 @@ public class Utils {
 
     public void Message(Player p, String msg) {
         p.sendMessage(ChatColor.translateAlternateColorCodes('&', msg));
+    }
+
+    public void Logger(Level level, String msg) {
+
     }
 }
